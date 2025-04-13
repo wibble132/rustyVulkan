a4d36e107ed24255165b5cb675aecd99498b7177
wibble <wibble132@gmail.com>
2025-04-13 22:54:50 +0100

    Shaders? (using rust!?)

--- crates/vk-triangle/build.rs
+++ crates/vk-triangle/build.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use spirv_builder::{MetadataPrintout, SpirvBuilder};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SpirvBuilder::new("../../crates/shaders", "spirv-unknown-vulkan1.2")
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
+    Ok(())
+}
